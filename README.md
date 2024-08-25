c8b8dbac6d38fb8c5eac76a5b14611dbffea9888
Talika Bajaj <bajajtalika2001@gmail.com>
2024-08-25 21:30:36 +0530

    Add files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Frontend Mentor - Expenses chart component
+
+![Design preview for the Expenses chart component coding challenge](./design/desktop-preview.jpg)
+
+## Hi! 👋
+
+This is my 37th Frontend Mentor Coding Challenge. 
+
+I completed a Frontend Mentor challenge where I built a bar chart component from scratch. The challenge involved using a local JSON file to dynamically populate the chart data. This project sharpened my skills in handling dynamic data and visualizing it effectively through CSS, improving my ability to create interactive and data-driven components.
+
+
+**Had fun building this!** 🚀
---
+++ script.js
@@ -0,0 +1,35 @@
+const chart = document.querySelector('.chart');
+
+async function fetchData(params) {
+    let response = await fetch('/data.json')
+    let data = await response.json();
+    // console.log(data);
+    renderData(data);
+}
+
+function renderData(data) {
+    data.forEach((element, index) => {
+        let barHeight = parseFloat(element.amount) * 2.75;
+        console.log(Math.round(barHeight));
+        
+        chart.innerHTML += `
+        <div class="graph">
+            <p class="show-amt">$${element.amount}</p>
+            <div class="bar" id='bar-${index + 1}' style='height: ${Math.round(barHeight)}px' onmouseover='displayAmount(this)' onmouseout='hideAmount(this)'></div>
+        <p>${element.day}</p>
+        </div>
+        `
+        });
+}
+
+fetchData();
+
+//Function to display amount overhead
+function displayAmount(x) {
+    x.previousElementSibling.style.opacity = '1';
+}
+
+//Function to hide amount overhead
+function hideAmount(x) {
+    x.previousElementSibling.style.opacity = '0';
+}
